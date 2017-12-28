afcb4bf4ec0a24d6d1fa65e5fda34bf4be6ed1fa
151250007 <151250007@smail.nju.edu.cn>
2017-12-28 22:23:09 +0800

    试着创建了一个 maven 项目

---
+++ SOAP/src/main/java/edu/nju/scoreQuery/servlets/Client.java
@@ -0,0 +1,97 @@
+package edu.nju.scoreQuery.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.messaging.ReqRespListener;
+import javax.xml.namespace.QName;
+import javax.xml.soap.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
+@WebServlet("/Client")
+public class Client extends HttpServlet implements ReqRespListener {
+
+    private static final long serialVersionUID = 1L;
+
+    private SOAPConnection connection;
+    private static MessageFactory messageFactory = null;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        try {
+            //创建连接工厂
+            SOAPConnectionFactory connectionFactory = SOAPConnectionFactory.newInstance();
+            messageFactory = MessageFactory.newInstance();
+            connection = connectionFactory.createConnection();
+        } catch (UnsupportedOperationException e) {
+            e.printStackTrace();
+        } catch (SOAPException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //使用传递学号给用户
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>"
+                + "<p name='tip'>请输入学号</p>");
+
+        out.println(
+                "<form method='POST' action='"
+                        + response.encodeURL(request.getContextPath() + "/Receiver")
+                        + "'>");
+        out.println(
+                " <input type='text' name='number' value=''>");
+        out.println("<input type='submit' name='Submit' value='查询'>");
+        out.println("</form></body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    public SOAPMessage onMessage(SOAPMessage msg) {
+        SOAPPart soappart = msg.getSOAPPart();
+        try {
+            SOAPEnvelope incomingEnvelope;
+            incomingEnvelope = soappart.getEnvelope();
+            SOAPBody body = incomingEnvelope.getBody();
+
+            String ns = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+            QName eName = new QName(ns, "RDF", "rdf");
+            SOAPBodyElement bodyElement = body.addBodyElement(eName);
+            Iterator<SOAPElement> iterator = bodyElement.getChildElements();
+            while (iterator.hasNext()) {
+                SOAPElement soapElement = iterator.next();
+                String scoreType = soapElement.getAttribute("成绩性质");
+                String courseID = soapElement.getAttribute("课程编号");
+                String score = soapElement.getValue();
+                System.out.println("课程编号：" + courseID + " 成绩性质：" + scoreType + " 得分：" + score);
+            }
+            SOAPElement element;
+            element = (SOAPElement) iterator.next();
+            SOAPMessage message = messageFactory.createMessage();
+            SOAPEnvelope envelope = message.getSOAPPart().getEnvelope();
+
+            SOAPBody responsebody = envelope.getBody();
+            String responseText = "Got the SOAP message indicating there are " + element.getValue() + " laptops available.";
+            responsebody.addChildElement(envelope.createName("Response")).addTextNode(responseText);
+            return message;
+        } catch (SOAPException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+}  
---
+++ SOAP/src/main/java/edu/nju/scoreQuery/servlets/Receiver.java
@@ -0,0 +1,128 @@
+package edu.nju.scoreQuery.servlets;
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.activation.DataHandler;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.soap.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Servlet implementation class showMyOrderServlet
+ */
+@WebServlet("/Receiver")
+public class Receiver extends HttpServlet {
+    private String ns = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+    private String wsdlUrl = "http://localhost:8989/ms?wsdl";
+    private static final long serialVersionUID = 1L;
+    private SOAPConnection connection;
+    private SOAPElementFactory factory;
+
+    public Receiver() {
+        super();
+    }
+
+    public void init() {
+        try {
+            factory = SOAPElementFactory.newInstance();
+            SOAPConnectionFactory connectionFactory = SOAPConnectionFactory.newInstance();
+            connection = connectionFactory.createConnection();
+        } catch (UnsupportedOperationException e) {
+            e.printStackTrace();
+        } catch (SOAPException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String number = request.getParameter("number");//学号
+
+        try {
+            MessageFactory messageFactory = MessageFactory.newInstance();
+            SOAPMessage outgoingMessage = messageFactory.createMessage();
+            SOAPPart soappart = outgoingMessage.getSOAPPart();
+            SOAPEnvelope envelope = soappart.getEnvelope();
+            SOAPHeader header = envelope.getHeader();
+            SOAPBody body = envelope.getBody();
+            QName eName = new QName(ns, "RDF", "rdf");   //<nn:add xmlns="ns" />
+            SOAPBodyElement bodyElement = body.addBodyElement(eName);
+
+            NodeList scoreList = readXML(number);
+            for (int i = 0; i < scoreList.getLength(); i++) {//遍历节点
+                Element element = (Element) scoreList.item(i);
+                String scoreType = element.getAttribute("成绩性质");
+                String course = element.getAttribute("课程编号");
+                String score = element.getElementsByTagName("得分").item(0).getFirstChild().getNodeValue();
+                SOAPElement scoreElement = bodyElement.addChildElement("课程成绩");
+                scoreElement.setValue(score);
+                scoreElement.addAttribute(envelope.createName("成绩性质"), scoreType);
+                scoreElement.addAttribute(envelope.createName("课程编号"), course);
+            }
+            outgoingMessage.saveChanges();
+            outgoingMessage.writeTo(System.out);
+            System.out.println("\n invoking......");
+
+            StringBuffer serverUrl = new StringBuffer();
+            serverUrl.append(request.getScheme()).append("://").append(request.getServerName());
+            serverUrl.append(":").append(request.getServerPort()).append(request.getContextPath());
+            System.out.print("serverUrl: " + serverUrl);
+
+
+            String baseUrl = serverUrl.toString();
+            URL url = new URL(baseUrl + "/test.html");
+
+            AttachmentPart attachmentpart = outgoingMessage.createAttachmentPart(new DataHandler(url));
+            attachmentpart.setContentType("text/html");
+            outgoingMessage.addAttachmentPart(attachmentpart);
+
+            URL client = new URL(baseUrl + "/Client");
+            SOAPMessage incomingMessage = connection.call(outgoingMessage, client);
+            if (incomingMessage != null) {
+                incomingMessage.writeTo(System.out);
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    private static NodeList readXML(String number) throws Exception {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document document = db.parse(new File(Client.class.getResource("StudentList.xml").toURI()));
+        NodeList list = document.getElementsByTagName("学生信息");
+        NodeList scoreList = null;
+        for (int i = 0; i < list.getLength(); ++i) {
+            Element element = (Element) list.item(i);
+            String content = element.getElementsByTagName("学号").item(0)
+                    .getFirstChild().getNodeValue();
+            if (!content.equals(number)) {
+                continue;
+            }
+            scoreList = element.getElementsByTagName("课程成绩");
+            System.out.print(scoreList.getLength());
+        }
+        return scoreList;
+    }
+}
